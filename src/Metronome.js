8ef40581da381a547d02f671ed9307ef8d9f9811
subodhss23@gmail.com <subodhss23@gmail.com>
2019-07-11 12:59:25 -0700

    almost there

--- src/Metronome.js
+++ src/Metronome.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import './Metronome.css';
 
+import click1 from './click1.wav';
+import click2 from './click2.wav';
+
 class Metronome extends Component{
   constructor(props){
     super(props);
@@ -11,11 +14,58 @@ class Metronome extends Component{
       bpm: 100,
       beatsPerMeasure: 4
     };
+
+    //creating audio objects with the files 
+    this.click1 = new Audio(click1);
+    this.click2 = new Audio(click2);
+    this.startStop = this.startStop.bind(this);
+    this.handleBpmChange = this.handleBpmChange.bind(this);
+    this.playClick = this.playClick.bind(this);
   }
 
-  handleBpmChange = event => {
+  //handeling bmp 
+  handleBpmChange(event) {
     const bpm = event.target.value;
     this.setState({ bpm });
+  }
+
+  startStop(){
+   if (this.state.playing){
+     // stop the timer
+     clearInterval(this.timer);
+     this.setState({
+       playing: false
+     });
+   } else {
+     //start a timer with a current BPM
+     this.timer = setInterval(
+       this.playClick,
+       (60 / this.state.bpm) * 1000
+     )
+     this.setState(
+       {count : 0,
+        playing: true
+        // play a click "immdeiately" (afte setState finishes)
+      },
+      this.playClick
+     );
+   }
+  }
+
+  playClick(){
+      const { count, beatsPerMeasure } = this.state;
+
+      // The first bear will have a different sound than that others
+      if ( count % beatsPerMeasure === 0){
+        this.click2.play();
+      } else {
+        this.click1.play();
+      }
+
+      //leep track of which bear we're on
+      this.setState(state =>({
+        count: (state.count + 1) % this.state.beatsPerMeasure
+      }))
   }
 
   render(){
@@ -33,7 +83,9 @@ class Metronome extends Component{
               onChange = {this.handleBpmChange}
               />
         </div>  
-        <button>{playing ? 'Stop' : 'Start'} </button>
+        <button onClick={this.startStop}> 
+                {playing ? 'Stop' : 'Start'} 
+        </button>
       </div>
     );
   }
